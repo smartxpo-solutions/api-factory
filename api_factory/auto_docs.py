0949d2aa4c32c42068f0feb27ba1e2a851d1a86e
udeos <ruzhitskiy.bogdan@gmail.com>
2019-04-15 13:50:34 +0300

    auto documentation feature

---
+++ api_factory/auto_docs.py
@@ -0,0 +1,66 @@
+from typing import List, Dict
+
+from .handlers import BaseHandler
+from .fields import (BaseField, SubformField, ListSubformField, DictSubformField, StringField,
+                     BooleanField, NumericField, FloatField, IntegerField, ListField, DictField, JSONField)
+
+
+def get_field_type(field) -> str:
+    if isinstance(field, StringField):
+        return 'str'
+    if isinstance(field, BooleanField):
+        return 'bool'
+    if isinstance(field, FloatField):
+        return 'float'
+    if isinstance(field, IntegerField):
+        return 'int'
+    if isinstance(field, JSONField):
+        return 'json'
+    if isinstance(field, ListField):
+        return 'list[%s]' % get_field_type(field.field_type)
+    if isinstance(field, DictField):
+        return 'dict[%s]' % get_field_type(field.field_type)
+    if isinstance(field, ListSubformField):
+        return 'list subform'
+    if isinstance(field, DictSubformField):
+        return 'dict subform'
+
+
+def get_field_info(field: BaseField) -> Dict:
+    docs = {
+        'description': field.help,
+        'required': field.required,
+    }
+
+    details = {'type': get_field_type(field)}
+    if isinstance(field, SubformField):
+        details['subform'] = get_field_info(field.subform)
+    else:
+        details['default'] = field.default
+        if isinstance(field, StringField):
+            details = {'min_length': field.min_length, 'max_length': field.max_length}
+        elif isinstance(field, NumericField):
+            details = {'min_value': field.min_value, 'max_value': field.max_value}
+        elif isinstance(field, ListField):
+            details = {'blank': field.blank, 'valid_values': field.valid_values}
+        elif isinstance(field, DictField):
+            details = {'blank': field.blank}
+
+    docs['details'] = details
+    return docs
+
+
+def collect_forms_documentation(form) -> Dict:
+    if form is not None:
+        return {key: get_field_info(field) for key, field in form.fields}
+
+
+def collect_documentation(handlers: List[BaseHandler]) -> List[Dict]:
+    return [
+        {
+            'http_method': handler.method,
+            'description': handler.help,
+            'input': collect_forms_documentation(handler.input_form),
+            'output': collect_forms_documentation(handler.output_form)
+        } for handler in handlers
+    ]
---
+++ api_factory/tests/test_auto_docs.py
@@ -0,0 +1,55 @@
+from unittest import TestCase
+
+from ..handlers import BaseHandler
+from ..forms import BaseInputForm, BaseOutputForm
+from ..fields import StringField, IntegerField, BooleanField, ListField
+from ..auto_docs import collect_documentation
+
+
+class TestAutoDocs(TestCase):
+    maxDiff = None
+
+    def test_simple_docs(self):
+
+        class TestOutputForm(BaseOutputForm):
+            is_sent = BooleanField(help='Was the letter sent?')
+
+        class SendEventReviewEmailInput(BaseInputForm):
+            event_id = StringField(required=True, help='Event ID')
+            edition_id = StringField(required=True, help='Edition ID')
+            emails = ListField(StringField, required=True, blank=False, help='Receivers')
+            body = StringField(required=True, min_length=5, help='Message body')
+
+        class SendEventReviewEmailHandler(BaseHandler):
+            help = 'Send a event review message to the event users'
+            input_form = SendEventReviewEmailInput
+            output_form = TestOutputForm
+            method = 'POST'
+
+            def handler(self):
+                pass
+
+        docs = collect_documentation([SendEventReviewEmailHandler])
+        self.assertEqual(len(docs), 1)
+
+        expected_docs = {
+            'description': 'Send a event review message to the event users',
+            'http_method': 'POST',
+            'input': {
+                'body': {'description': 'Message body',
+                         'details': {'max_length': None, 'min_length': 5},
+                         'required': True},
+                'edition_id': {'description': 'Edition ID',
+                               'details': {'max_length': None, 'min_length': None},
+                               'required': True},
+                'emails': {'description': 'Receivers',
+                           'details': {'blank': False, 'valid_values': None},
+                           'required': True},
+                'event_id': {'description': 'Event ID',
+                             'details': {'max_length': None, 'min_length': None},
+                             'required': True}},
+            'output': {
+                'is_sent': {'description': 'Was the letter sent?',
+                            'details': {'default': None, 'type': 'bool'},
+                            'required': False}}}
+        self.assertDictEqual(docs[0], expected_docs)
